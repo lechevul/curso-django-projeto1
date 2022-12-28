32c30db913661220223e0bdb0a14bdf59ccc2a43
Andre Nascimento <lechevul@gmail.com>
2022-12-28 17:50:19 -0300

    aula 42

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Andre',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Andre',
+    })
